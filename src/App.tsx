c6756f1adba345d8dc9cedbd521c0799d7c47bac
Hiro_Minami <hiro.0907@icloud.com>
2021-09-23 12:21:21 +0900

    Drag & Dropのアニメーション作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import TaskContainer from "./components/TaskContainer";
 import { AppContext } from "./contexts/AppContexts";
 import reducer from "./reducers";
 import { manager } from "./models";
+import "./app.css";
 
 export default function App() {
     const [state, dispatch] = useReducer(reducer, manager);
